2a6cb9c0efe8a8b808f4d35a5d3de9a874114ad7
Kürşat <66494923+KursatBB@users.noreply.github.com>
2023-02-20 15:29:04 +0300

    Tor setup fix

--- ktoolsetup.py
+++ ktoolsetup.py
@@ -140,6 +140,6 @@ def torBrowser():
         print("Could not find download link")
     else:
         download_url = download_links[0]['href']
-        print(f"Downloading from {'www.torproject.org/'+download_url}")
-        urllib.request.urlretrieve('www.torproject.org/'+download_url, "torbrowser-install.tar.xz")
+        print(f"Downloading from {'https://www.torproject.org'+download_url}")
+        urllib.request.urlretrieve('https://www.torproject.org'+download_url, "torbrowser-install.tar.xz")
         os.system("tar -xf torbrowser-install.tar.xz")
